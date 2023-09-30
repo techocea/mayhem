d450e93db46c94c2774a615b0b8ede57c887a1b6
Brightly Dunsford <131114729+techocea@users.noreply.github.com>
2023-09-30 21:18:55 +0530

    second commit

--- src/components/HeaderTop.js
+++ src/components/HeaderTop.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BsFacebook, BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
 
+import Link from "next/link";
+
 const HeaderTop = () => {
   return (
     <div className="flex max-sm:flex-col max-sm:gap-4 max-sm:text-center items-center justify-between p-5 border-b border-gray-500 ">
       <div className="flex gap-8">
-        <BsFacebook className="hover:text-accent duration-75" />
+        <Link href="https://web.facebook.com/profile.php?id=61550819112955">
+          <BsFacebook className="hover:text-accent duration-75" />
+        </Link>
         <BsTwitter className="hover:text-accent duration-75" />
         <BsInstagram className="hover:text-accent duration-75" />
         <BsLinkedin className="hover:text-accent duration-75" />
